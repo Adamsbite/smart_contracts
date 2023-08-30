bda440cab5e41fd17bbae159e5d7aa5a8694e9a5
Vintage <btadamz@gmail.com>
2023-08-30 23:56:23 +0100

    Update function.sol

--- function.sol
+++ function.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 contract menty {
-    // The contract name is "menty"
+    // The contract name is "menty"  
 
     // The function name is "add", and it takes two input arguments: x and y of type uint (unsigned integer)
     // The "pure" keyword indicates that this function does not modify the contract's state and only returns a value
